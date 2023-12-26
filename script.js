e5453695cb53142a7ee7288f322bcfa1d86cb1ed
Muhammad Susilo <muhammadsusilo43@gmail.com>
2023-12-26 15:45:50 +0700

    memperbaiki file css dan menambahkan validasi email

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function animasi() {
-    const arrows = document.querySelectorAll(".bxs-down-arrow-circle")
+    const arrows = document.querySelectorAll(".bxs-down-arrow-circle");
 
     arrows.forEach(el => {
         el.addEventListener("click", function() {
@@ -7,15 +7,38 @@ function animasi() {
             const input = el.previousElementSibling;
             const parent = el.parentElement;
             const email = parent.nextElementSibling;
-            console.info(email);
-        })
+            // console.info(email);
+
+            // check validasi
+            if (input.type === "text" && validasiUser(input)) {
+                // console.log("terima kasih ok")
+                next(parent, email)
+            }
+        });
     })
 };
 
-function validasi(user) {
+function validasiUser(user) {
+    if (user.value.length < 6) {
+        console.log(`Menulis tidak sesuai karakter`);
 
+        error(`rgb(189,87,87)`);
+    } else {
+        error(`rgb(176,176,176)`);
+        return true;
+
+    }
 }
 
+function next(parent, email) {
+    parent.classList.add("innactive");
+    parent.classList.remove("active");
+    email.classList.remove("active");
+}
+
+function error(color) {
+    document.body.style.backgroundColor = color;
+}
 
 
 animasi();
